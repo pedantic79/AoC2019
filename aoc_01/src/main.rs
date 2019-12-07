77d72b6697a74281fb4c51660cedc0cb423c0ece
Dan Nguyen <pedantic@gmail.com>
2019-12-07 01:25:26 -0600

    Add Advent of Code Day 1

---
+++ aoc_01/src/main.rs
@@ -0,0 +1,74 @@
+use std::fs::File;
+use std::io::Read;
+use std::iter::from_fn;
+
+fn main() {
+    let mut file = File::open("input.txt").expect("unable to open input.txt");
+    let v = read_input(&mut file).expect("parse error");
+
+    let total: u32 = v.iter().map(|&x| fuel(x)).sum();
+    println!("Total fuel: {}", total);
+
+    let real_total: u32 = v.iter().map(|&x| fuel_of_fuel(x)).sum();
+    println!("Total fuel of fuel: {}", real_total);
+}
+
+fn fuel(mass: u32) -> u32 {
+    (mass / 3).saturating_sub(2)
+}
+
+fn fuel_of_fuel(mass: u32) -> u32 {
+    let mut current_mass = mass;
+
+    from_fn(move || {
+        let current_fuel = fuel(current_mass);
+        if current_fuel == 0 {
+            None
+        } else {
+            current_mass = current_fuel;
+            Some(current_fuel)
+        }
+    })
+    .sum()
+}
+
+fn read_input<R: Read>(input: &mut R) -> Result<Vec<u32>, String> {
+    let mut buffer = String::new();
+    if let Err(msg) = input.read_to_string(&mut buffer) {
+        return Err(msg.to_string());
+    }
+
+    buffer
+        .lines()
+        .map(|s| {
+            s.parse::<_>()
+                .map_err(|e: std::num::ParseIntError| format!("{}: {}", e, s))
+        })
+        .collect::<Result<_, _>>()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn problem_part1() {
+        assert_eq!(fuel(12), 2);
+        assert_eq!(fuel(14), 2);
+        assert_eq!(fuel(1969), 654);
+        assert_eq!(fuel(100_756), 33583);
+    }
+
+    #[test]
+    fn parsing_file() {
+        let mut input: &[u8] = b"123\n9\n45678\n12\n234\n";
+        assert_eq!(read_input(&mut input), Ok(vec![123, 9, 45678, 12, 234]));
+    }
+
+    #[test]
+    fn problem_part2() {
+        for case in [(14, 2), (1969, 966), (100_756, 50346)].iter() {
+            assert_eq!(fuel_of_fuel(case.0), case.1);
+        }
+    }
+}
